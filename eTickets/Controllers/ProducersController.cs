bc82ec1a433186f0053b71ee6f30f47a8f14086c
ayoubzann <ayoub@gmail.com>
2022-11-08 08:39:48 +0100

    Add project files.

---
+++ eTickets/Controllers/ProducersController.cs
@@ -0,0 +1,92 @@
+﻿using eTickets.Data;
+using eTickets.Data.Services;
+using eTickets.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly IProducersService _service;
+        public ProducersController(IProducersService service)
+        {
+          _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _service.GetAllAsync();
+            return View("Index", allProducers);
+        }
+
+        //GET: producers/details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+
+        //GET: producers/create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ProfilePictureUrl,FullName,Bio")] Producer producer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(producer);
+            }
+            await _service.AddAsync(producer);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET: producers/edit/4
+        public async Task<IActionResult> Edit(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProfilePictureUrl,FullName,Bio")] Producer producer)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(producer);
+            }
+            if(id == producer.Id)
+            {
+                await _service.UpdateAsync(id, producer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producer);
+        }
+
+        //GET: producers/delete/4
+        public async Task<IActionResult> Delete(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ eTickets/Models/Cinema.cs
@@ -0,0 +1,30 @@
+﻿using eTickets.Data.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Cinema : IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+        
+
+        [Display(Name = "Cinema Logo")]
+        [Required(ErrorMessage = "Cinema Logo is required.")]
+        public string Logo { get; set; }
+
+        [Display(Name = "Cinema Name")]
+        [Required(ErrorMessage = "Name is required.")]
+
+        public string Name { get; set; }
+
+        [Display(Name = "Cinema Description")]
+        [Required(ErrorMessage = "Cinema description is required.")]
+
+        public string Description { get; set; }
+
+        //Relations
+
+        public List<Movie> Movies { get; set; }
+    }
+}
